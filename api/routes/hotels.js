d2fdf40c3f5724dffd8252dc3735056a583a09b7
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-23 18:17:15 +0100

    hotel route

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,7 +1,17 @@
 import express from "express"
+import hotel from "../models/hotel.js";
 
 const router = express.Router();
 
-router.post("/", (req, res) => {})
+router.post("/", async (req, res) => {
+    const newHotel = new hotel(req.body)
+
+    try{
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 export default router
