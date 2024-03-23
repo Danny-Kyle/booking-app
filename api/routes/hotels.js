17704f414e3628a6eec490f462f4bca442136c9a
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-23 20:11:00 +0100

    update hotel

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,13 +2,23 @@ import express from "express"
 import hotel from "../models/hotel.js";
 
 const router = express.Router();
-
+//creating
 router.post("/", async (req, res) => {
     const newHotel = new hotel(req.body)
 
     try{
         const savedHotel = await newHotel.save()
         res.status(200).json(savedHotel)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//updating
+router.put("/:id", async (req, res) => {
+    try{
+        const updatedHotel = await hotel.findByIdAndUpdate(req.params.id, {$set: req.body});
+        res.status(200).json(updatedHotel);
     }catch(err){
         res.status(500).json(err)
     }
