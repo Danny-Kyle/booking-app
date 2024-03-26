113ec32c4348a57142fa5b1a779df2a52e11a3be
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-26 08:04:40 +0100

    hotel controller

---
+++ api/controllers/hotelController.js
@@ -0,0 +1,51 @@
+import hotel from "../models/hotel.js";
+
+export const createHotel = async (req, res, next) => {
+  const newHotel = new hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateHotel = async (req, res, next) => {
+    try{
+        const updatedHotel = await hotel.findByIdAndUpdate(
+            req.params.id,
+            {$set: req.body},
+            {new: true}
+            );
+        res.status(200).json(updatedHotel);
+    }catch(err){
+        next(err);
+    }
+};
+
+export const deleteHotel  = async (req, res, next) => {
+    try{
+        await hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel has been deleted successfully!");
+    }catch(err){
+        next(err);
+    }
+}
+
+export const getSingleHotel = async (req, res, next) => {
+    try{
+        const hotel = await hotel.findById(req.params.id);
+        res.status(200).json(hotel);
+    }catch(err){
+        next(err);
+    }
+}
+
+export const getAllHotels = async (req, res, next) => {
+    try{
+        const hotels = await hotel.find();
+        res.status(200).json(hotels);
+    }catch(err){
+        next(err)
+    }
+}
