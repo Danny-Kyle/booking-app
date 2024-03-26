82c6ff6f9e3e5af2270b95ee1a55a0538bbf8f5c
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-26 08:05:31 +0100

    auth controller

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -7,7 +7,6 @@ export const register = async(req, res, next) => {
             email: req.body.email,
             password: req.body.password
         })
-
         await newUser.save()
         res.status(200).send("new user created successfully")
     }catch(err){
