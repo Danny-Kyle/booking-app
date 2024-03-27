4571076993141fd44c02411c72f4fbd8f1087180
Danny-Kyle <dikedaniel7917@gmail.com>
2024-03-27 11:26:28 +0100

    wgo?

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -2,11 +2,16 @@ import user from "../models/user"
 
 export const register = async(req, res, next) => {
     try{
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+
         const newUser = new user({
             username: req.body.username,
             email: req.body.email,
             password: req.body.password
-        })
+        });
+        
         await newUser.save()
         res.status(200).send("new user created successfully")
     }catch(err){
