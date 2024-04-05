5530d2333d495993cec6fa2b785a09e346287d55
Danny-Kyle <dikedaniel7917@gmail.com>
2024-04-05 16:32:57 +0100

    update hotel model

--- api/models/hotel.js
+++ api/models/hotel.js
@@ -4,47 +4,50 @@ const { schema }  = mongoose;
 const HotelSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
     },
     type: {
         type: String,
-        required: true
+        required: true,
     },
     city: {
         type: String,
-        required: true
+        required: true,
     },
     address: {
         type: String,
-        required: true
+        required: true,
     },
     distance: {
         type: String,
-        required: true
+        required: true,
     },
     photos: {
         type: [String],
     },
     desc: {
         type: String,
-        required: true
+        required: true,
     },
     rating: {
         type: Number,
         min: 0,
-        max: 5
+        max: 5,
     },
     rooms:{
         type: [String],   
     },
     cheapestPrice: {
         type: Number,
-        required: true
+        required: true,
     },
     featured:{
         type: Boolean,
-        default: false
+        default: false,
     }
 })
 
-export default mongoose.model("Hotel", HotelSchema)
+// export default mongoose.model("Hotel", HotelSchema)
+
+const Hotel = mongoose.model("Hotel", HotelSchema)
+export default Hotel
