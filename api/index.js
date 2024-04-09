e3b947de7f6547a12fe18eddef9c5658dad9b76e
Danny-Kyle <dikedaniel7917@gmail.com>
2024-04-09 15:59:57 +0100

    index update

--- api/index.js
+++ api/index.js
@@ -6,47 +6,47 @@ import roomRoute from "./routes/rooms.js"
 import hotelRoute from "./routes/hotels.js"
 import userRoute from "./routes/users.js"
 import cookieParser from "cookie-parser"
-const app = express()
-const port = 5000
-dotenv.config()
+const app = express();
+const port = 5000;
+dotenv.config();
 
 const connect = async() => {
     try{
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected to MongoDB")
+        console.log("Connected to MongoDB");
     }catch(error){
-        throw error
+        throw error;
     }   
 }
 
 //middlewares
-app.use(cookieParser())
-app.use(express.json())
-app.use("/api/auth", authRoute)
-app.use("/api/users", userRoute)
-app.use("/api/hotels", hotelRoute)
-app.use("/api/rooms", roomRoute)
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/hotels", hotelRoute);
+app.use("/api/rooms", roomRoute);
 
 app.use((err, req, res, next) => {
-    const errorStatus = err.status || 500
-    const errorMessage = err.message || "Something ain't right"
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something ain't right";
     return res.status(errorStatus).json({
         success: false,
         status: errorStatus,
         message: errorMessage,
         stack: err.stack,
     })
-})
+});
 
 mongoose.connection.on("disconnected", () => {
-    console.log("Disconnected from MongoDB")
-})
+    console.log("Disconnected from MongoDB");
+});
 
 app.get("/users", (req, res) => {
-    res.send("hello first request")
-})
+    res.send("hello first request");
+});
 
 app.listen(port, () => {
     connect()
-    console.log('listening on port' + port)
-})
+    console.log('listening on port' + port);
+});
