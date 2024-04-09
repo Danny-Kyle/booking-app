bf22b1b6fc099494314efd4091800854b6dab3a6
Danny-Kyle <dikedaniel7917@gmail.com>
2024-04-09 15:58:27 +0100

    verify Token utility created

---
+++ api/utils/verifyToken.js
@@ -0,0 +1,29 @@
+import jwt, { verify } from "jsonwebtoken"
+import { createError } from "./error"
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    if(!token) {
+        return next(createError(401, "you're not authenticated!"));
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if(err){
+            return next(createError(403, "Token not valid!"));
+        }
+        req.user = user;
+        next();
+    })
+}
+
+export const verifyUser  = (req, res, next) => {
+    verifyToken(res, req, next, ()=>{
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next();
+        }else{
+            if(err){
+                return next(createError(403, "You are not authorized!"))
+            }
+        }
+    } )
+}
