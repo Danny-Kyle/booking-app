49b74d186eaaf5101b5c7e3514de5c200bd37535
Danny-Kyle <dikedaniel7917@gmail.com>
2024-04-09 15:59:12 +0100

    user route updated

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,21 @@
 import express from "express"
+import { createUser, deleteUser, getAllUsers, getSingleUser, updateUser } from "../controllers/userController";
+import { verifyToken, verifyUser } from "../utils/verifyToken";
 
 const router = express.Router();
 
-export default router
+router.get("/checkauthentication", verifyToken, (res, req, next) => {
+    res.send("Hello User, Log IN Successful!")
+})
+
+router.get("/checkuser/:id", verifyUser, (res, req, next) => {
+    res.send("Hello User, Log IN Successful and account termination is possible!")
+})
+
+router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.get("/", getAllUsers);
+router.get("/:id", getSingleUser);
+
+export default router;
