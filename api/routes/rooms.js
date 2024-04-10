3c37cf903ccda334af282a7a5e38f59a87589765
Danny-Kyle <dikedaniel7917@gmail.com>
2024-04-10 18:27:42 +0100

    update room route

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,21 @@
 import express from "express"
+import { createRoom, deleteRoom, getAllRooms, getSingleRoom, updateRoom } from "../controllers/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+//creating
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//updating
+router.put("/:id", verifyAdmin, updateRoom)
+
+//deleting
+router.put("/:id", verifyAdmin, deleteRoom);
+
+// show/GET single Room
+router.get("/:id", getSingleRoom);
+
+// show/GET all Rooms
+router.get("/", getAllRooms);
 export default router
