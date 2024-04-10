c8e8f77592b4eee600998c7b3ed6876d872ca10c
Danny-Kyle <dikedaniel7917@gmail.com>
2024-04-10 13:39:05 +0100

    update room model

--- api/models/room.js
+++ api/models/room.js
@@ -1,7 +1,26 @@
 import mongoose from "mongoose";
 const RoomSchema = mongoose(
     {
-        
+        title:{
+            type: String,
+            required: true,
+        },
+        price:{
+            type: Number,
+            required: true,
+        },
+        maxPeople:{
+            type: Number,
+            required: true,
+        },
+        desc: {
+            type: String,
+            required: true,
+        },
+        roomNumber: [{
+            number: Number,
+            unavailableDates: [{type: Date}]
+        }]
     }
 )
 
