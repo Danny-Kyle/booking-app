7c424986196a3d81de1bad022e6b06ecbfb90dc6
Danny-Kyle <dikedaniel7917@gmail.com>
2024-04-17 12:57:38 +0100

    update hotel controller

--- api/controllers/hotelController.js
+++ api/controllers/hotelController.js
@@ -1,3 +1,4 @@
+import Hotel from "../models/hotel.js";
 import hotel from "../models/hotel.js"; 
 import { createError } from "../utils/error.js";
 
@@ -46,6 +47,30 @@ export const getAllHotels = async (req, res, next) => {
     try{
         const hotels = await hotel.find();
         res.status(200).json(hotels);
+    }catch(err){
+        next(err)
+    }
+}
+
+export const countByCity = async(req, res, next) => {
+    const cities = req.query.cities.split(",")
+    try{
+        const list = await Promise.all(cities.map(city => {
+            return Hotel.countDocuments({city: city})
+        }))
+        res.status(200).json(list);
+    }catch(err){
+        next(err)
+    }
+}
+
+export const countByType = async(req, res, next) => {
+    const types = req.query.types.split(",")
+    try{
+        const list = await Promise.all(types.map(city => {
+            return Hotel.countDocuments({city: city})
+        }))
+        res.status(200).json(list);
     }catch(err){
         next(err)
     }
